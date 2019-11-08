c302418657c29f6e2c8c5542066faa225b76b1bf
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-11-07 22:34:41 -0300

    avance, ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tallerdeajedrez';
+
+  onActivate(event: any) {
+    window.scroll(0, 0);
+    // or document.body.scrollTop = 0;
+    // or document.querySelector('body').scrollTo(0,0)
+    // https://stackoverflow.com/questions/48048299/angular-5-scroll-to-top-on-every-route-click/48048822
+  }
+
+  // ¡ PUEDE SERVIR PARA HACER SCROLL-TOP CON UN BUTTON !
+  /* onActivate(event: any) {
+    const scrollToTop = window.setInterval(() => {
+        const pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+  } */
+
 }
--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -12,4 +12,15 @@ export class FooterComponent implements OnInit {
   ngOnInit() {
   }
 
+  scrollTop(event: any) {
+    const scrollToTop = window.setInterval(() => {
+        const pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+  }
+
 }
