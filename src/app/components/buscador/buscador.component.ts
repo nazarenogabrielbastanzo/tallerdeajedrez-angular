22578a64f6c05d5da022b0cdb83f47ed7e5e1893
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-11-23 16:32:18 -0300

    PopOver en partidas y buscador, ok

--- src/app/components/buscador/buscador.component.ts
+++ src/app/components/buscador/buscador.component.ts
@@ -1,25 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { PeticionesService } from '../../services/peticiones.service';
-import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
-  styleUrls: ['./buscador.component.css'],
-  providers: [NgbTooltipConfig]
+  styleUrls: ['./buscador.component.css']
 })
 export class BuscadorComponent implements OnInit {
 
   partidas: any[] = []; // es arreglo porque pueden ser varias las coincidencias que aparezcan
   termino: string;
-  srcImgTooltip: string;
+  srcImgPop: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private peticionesService: PeticionesService,
-    private router: Router,
-    private config: NgbTooltipConfig
+    private router: Router
   ) {
     this.activatedRoute.params.subscribe((params: any) => {
       /* console.log( params.termino ); */
@@ -28,8 +25,6 @@ export class BuscadorComponent implements OnInit {
       this.partidas = this.peticionesService.buscarPartidas( params.termino );
       /* console.log(this.partidas); */
     });
-    this.config.placement = 'top';
-    this.config.container = 'body';
   }
 
   ngOnInit() {}
@@ -42,8 +37,8 @@ export class BuscadorComponent implements OnInit {
     this.router.navigate(['/partidas']);
   }
 
-  setSrcImgTooltip(numero: number) {
-    this.srcImgTooltip = `assets/images/nuevas/tooltips/${ numero }.png`;
+  setSrcImgPop(numero: number) {
+    this.srcImgPop = `assets/images/nuevas/tooltips/${ numero }.png`;
   }
 
 }
