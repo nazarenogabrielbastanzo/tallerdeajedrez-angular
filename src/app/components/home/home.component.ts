c9aa1c78f759d1b7c7b7849f1ac4fcd0059eca84
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2019-11-25 09:31:03 -0300

    Mejorado el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  color = false;
+  nro: number;
+
   tarjetas = [
     {
       img: 'assets/images/jorgeraulbastanzo.jpg',
