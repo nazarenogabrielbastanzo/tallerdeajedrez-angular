a6bf93341d74f760f749773749e7e756325d45d6
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-03-07 15:39:59 -0300

    Emprolijado el modulo, ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
