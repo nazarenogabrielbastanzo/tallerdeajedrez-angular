b2eafb692ff2e2cac26a3b3083898c728322c71d
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-04-26 12:32:45 -0300

    Update app.component.html and mi-modulo.module.ts

--- src/app/mi-modulo.module.ts
+++ src/app/mi-modulo.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 /* import {MatSnackBarModule} from '@angular/material/snack-bar'; */
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    MatToolbarModule
     /* MatSnackBarModule */
   ],
   exports: [
+    MatToolbarModule
     /* MatSnackBarModule */
   ]
 })
