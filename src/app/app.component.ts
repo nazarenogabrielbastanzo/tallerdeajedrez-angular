60c09f7d1d1a98f8dd7f90c7c9fea38350fb6ded
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-05-27 18:37:09 -0300

    Integración de Firebase Auth Anónimo correcto?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MessagingService } from './shared/messaging.service';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
@@ -9,18 +10,40 @@ import { MessagingService } from './shared/messaging.service';
 export class AppComponent implements OnInit {
 
   message: any;
+  isAnonymous: boolean;
+  uid: string;
 
   constructor(
-    private messagingService: MessagingService
-  ) {}
+    private messagingService: MessagingService,
+    private angularFireAuth: AngularFireAuth
+  ) { }
 
   ngOnInit(): void {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
-    const userId = 'user001';
-    this.messagingService.requestPermission(userId);
-    this.messagingService.receiveMessage();
-    this.message = this.messagingService.currentMessage;
+    this.angularFireAuth.auth.signInAnonymously()
+      .catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+      });
+
+    this.angularFireAuth.auth.onAuthStateChanged((user) => {
+        if (user) {
+          // User is signed in.
+          this.isAnonymous = user.isAnonymous;
+          this.uid = user.uid;
+          // ...
+          /* const userId = 'user001'; */
+          this.messagingService.requestPermission(this.uid);
+          this.messagingService.receiveMessage();
+          this.message = this.messagingService.currentMessage;
+        } else {
+          // User is signed out.
+          // ...
+        }
+        // ...
+      });
+
   }
 
   // https://stackoverflow.com/questions/48048299/angular-5-scroll-to-top-on-every-route-click/48048822
