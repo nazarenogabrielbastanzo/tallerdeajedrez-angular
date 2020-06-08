03b606bb23d6ce06908f8d5742ac72ebbed38b2f
nazarenogabrielbastanzo <b.nazarenogabriel@gmail.com>
2020-06-08 09:34:02 -0300

    Bindeada la cantidad de partidas con el navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '../../../services/auth.service';
+import { AuthService } from '../../../services/auth.service'; // el authservice de auth0
+import { AngularFireAuth } from '@angular/fire/auth';
+import { FirestoreService } from '../../../services/firestore/firestore.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,13 +13,45 @@ export class NavbarComponent implements OnInit {
 
   public isMenuCollapsed = true;
   open: boolean;
+  isAnonymous: boolean;
+  uid: string;
+  partidas: any;
 
   constructor(
     public router: Router,
-    public auth: AuthService
+    public auth: AuthService,
+    private angularFireAuth: AngularFireAuth,
+    private firestoreService: FirestoreService,
   ) { }
 
   ngOnInit() {
+    this.angularFireAuth.auth.signInAnonymously()
+      .catch((error) => {
+        console.log(error.code);
+        console.log(error.message);
+      });
+
+    this.angularFireAuth.auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        this.isAnonymous = user.isAnonymous;
+        this.uid = user.uid;
+        // ...
+        this.firestoreService.getPartidas().subscribe((partidasSnapshot) => {
+          this.partidas = [];
+          partidasSnapshot.forEach((partidaData: any) => {
+              this.partidas.push({
+                id: partidaData.payload.doc.id,
+                data: partidaData.payload.doc.data()
+              });
+          });
+        });
+      } else {
+        // User is signed out.
+        // ...
+      }
+      // ...
+    });
   }
 
   openNav() {
