a3c72c78fa395a04f481babd430c77f4cfd4a5c1
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-05-21 11:25:38 -0300

    Deleted torneos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,7 +28,7 @@ import { NgpSortModule } from 'ngp-sort-pipe';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { PartidasComponent } from './components/partidas/partidas.component';
-import { TorneosComponent } from './components/torneos/torneos.component';
+/* import { TorneosComponent } from './components/torneos/torneos.component'; */
 import { FotosComponent } from './components/fotos/fotos.component';
 import { FrasesComponent } from './components/frases/frases.component';
 import { DispositivosComponent } from './components/dispositivos/dispositivos.component';
@@ -60,7 +60,7 @@ import { SidenavComponent } from './components/shared/sidenav/sidenav.component'
     AppComponent,
     HomeComponent,
     PartidasComponent,
-    TorneosComponent,
+    /* TorneosComponent, */
     FotosComponent,
     FrasesComponent,
     DispositivosComponent,
