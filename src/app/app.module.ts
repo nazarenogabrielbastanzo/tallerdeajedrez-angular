beacbc387fd63f224260c4023e686a7d22697c48
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-10-17 20:56:15 -0300

    Limpieza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgMaterialModule } from './modules/ng-material/ng-material.module';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-import { AuthModule } from '@auth0/auth0-angular';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -15,21 +12,14 @@ import { AngularFireAuth } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { SidenavComponent } from './components/shared/sidenav/sidenav.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
-
-// import { OrderbyPipe } from './pipes/orderby.pipe';
-// import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 import { MessagingService } from './services/messaging.service';
 import { ScrollTopComponent } from './components/shared/scroll-top/scroll-top.component';
 
 @NgModule({
   declarations: [
-    // OrderbyPipe,
-    // DomseguroPipe,
     AppComponent,
-    SidenavComponent,
     HomeComponent,
     FooterComponent,
     ScrollTopComponent,
@@ -41,18 +31,6 @@ import { ScrollTopComponent } from './components/shared/scroll-top/scroll-top.co
     BrowserAnimationsModule,
     NgMaterialModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    // Import the module into the application, with configuration
-    AuthModule.forRoot({
-      domain: environment.auth0Config.domain,
-      clientId: environment.auth0Config.clientId
-    }),
-    // NgbModule,
-    // NgbPaginationModule,
-    // NgbAlertModule,
-  ],
-  exports: [
-    // OrderbyPipe,
-    // DomseguroPipe,
   ],
   providers: [
     AngularFirestore,
